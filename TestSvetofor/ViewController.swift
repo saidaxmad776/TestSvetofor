610e05d44f4cab69f32b937a98d41751bef7ec5c
ASaidaxmad <saidaxmad7760746@gmail.com>
2022-03-07 13:56:33 +0500

    Initial Commit

---
+++ TestSvetofor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestSvetofor
+//
+//  Created by Test on 07/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
